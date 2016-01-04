15e87c063b04548ae99c157b73ef7eb3ed59f9f8
Amador Alzaga Palacios <contact@amadoralzaga.com>
2016-01-04 01:09:54 +0100

    Update test-cam.py

--- test-cam.py
+++ test-cam.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-#This code only showws camera captured frames on a window if all works as expected
+# This code only showws camera captured frames on a window if all works as expected
+# An USB webcam like Logitech C270 must be connected on USB bus
 
 import cv2
 import numpy as np
@@ -10,20 +11,21 @@ if not cap.isOpened:
     print "Unable to open device"
     sys.exit(1)
 
+# Set frame size to 320 x 240 pixels
 cap.set(3, 320)
 cap.set(4, 240)
 
+# Main loop. Exits on error or ESC key pressed.
 while True:
     ret, img = cap.read()
     if not ret:
         print "Unable to retrieve frame from the device"
         sys.exit(1)
-    
-    hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        
     cv2.imshow('camera',img)
+    
     if cv2.waitKey(10) & 0xFF == 27:
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
